0f851841e996bc4ce2a7fc882e7dfc7a7fdacfbb
mstathopoulos <42986465+mstathopoulos@users.noreply.github.com>
2018-09-15 18:05:19 -0500

    Add files via upload
    Test of commit by uploading rather than pushing from eclipse

--- MyServlet.java
+++ MyServlet.java
@@ -35,6 +35,7 @@ public class MyServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		try {
+			
 			Class.forName("com.mysql.jdbc.Driver");
 			response.getWriter().println("To-Do List");
 		} 
